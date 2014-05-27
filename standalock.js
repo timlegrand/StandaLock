a727f7c9cba9028a2725c70baaa8574b37247cb6
manekinekko <maneki.nekko@gmail.com>
2014-05-28 00:23:33 +0200

    first commit

--- standalock.js
+++ standalock.js
@@ -1,204 +1,186 @@
-( function() {"use strict";
-        // this function is strict...
-    }()
-);
+"use strict";
 
-img = null;
-canvas = null;
-ctx = null;
-slider = null;
+function StandLock(config){
 
-// Graphics-dependant constants
-iHEIGHT = 68; // half-height, one bar only 
-iWIDTH = 469;
-cursor_radius = 13;
-x1 = 114 + (cursor_radius-1); // X position where the progress segment starts
-x2 = 445 - (cursor_radius-1); // X position where the progress segment ends
-w = x2 - x1;  // slider width
-y = 33;  // Y position of center of the slider
-x_text = x1 - 70;  // X position where to write text
-y_text = y + 7;
+  this.checkCanvasSupport();
 
-// Global state
-slider_value = 0.0; // represents percentage
-cursor_catched = false;
-passed = false; // prevents from running secured actions multiple times
+  this.decryptFn = config.decrypt;
+  this.template = config.template;
+  this.placeholder = document.querySelector(config.placeholder);
+  this.canvas = document.querySelector(config.renderer);
+  this.data = config.data;
+  this.ctx = this.canvas.getContext('2d');
+  this.canvas.addEventListener('mousedown', this._onmousedown.bind(this), false);
+  this.canvas.addEventListener('mousemove', this._onmousemove.bind(this), false);
+  this.canvas.addEventListener('mouseup', this._onmouseup.bind(this), false);
+  this.canvas.addEventListener('mouseout', this._onmouseout.bind(this), false);
 
+  // Global state
+  this._slider_value = 0.0; // represents percentage
+  this._cursor_catched = false;
+  this._passed = false; // prevents from running secured actions multiple times
 
-function drawBase(ctx) {
-
-    ctx.drawImage(img, 0, 0, iWIDTH, iHEIGHT, 0, 0, iWIDTH, iHEIGHT);
+  // Graphics-dependant constants
+  this._iHEIGHT = 68; // half-height, one bar only
+  this._iWIDTH = 469;
+  this._cursor_radius = 13;
+  this._x1 = 114 + (this._cursor_radius-1); // X position where the progress segment starts
+  this._x2 = 445 - (this._cursor_radius-1); // X position where the progress segment ends
+  this._w = this._x2 - this._x1;  // slider width
+  this._y = 33;  // Y position of center of the slider
+  this._x_text = this._x1 - 70;  // X position where to write text
+  this._y_text = this._y + 7;
+  this._sx = this._w * this._slider_value / 100;
 
 }
 
-function drawProgress(ctx) {
-   
-    // Calculated x position where the overalyed image should end
-    x_end = x1 + (w * slider_value) / 100; // relative to slidebar
-
-    ctx.drawImage(img, 0, iHEIGHT, x_end, iHEIGHT, 0, 0, x_end, iHEIGHT);
-
-    // Text to screen
-    ctx.fillStyle = "grey";
-    ctx.font = "14pt Arial";
-    ctx.fillText(Math.round(slider_value) + " %", x_text, y_text);
-
-    /* Draw cursor */
-    ctx.beginPath();
-    ctx.arc(x_end, y, cursor_radius, 0, 2 * Math.PI, false);
-    var radgrad = ctx.createRadialGradient(x_end, y, 0, x_end, y, cursor_radius);
-    radgrad.addColorStop(0, 'hsl(0,0%,85%)');
-    radgrad.addColorStop(0.7, 'hsl(0,0%,80%)');
-    radgrad.addColorStop(0.9, 'hsl(0,0%,80%)');
-    radgrad.addColorStop(1, 'hsl(0,0%,65%)');
-    ctx.fillStyle = radgrad;
-    ctx.fill();
+StandLock.prototype.checkCanvasSupport = function(){
+  try {
+    document.createElement('canvas').getContext('2d');
+  }
+  catch(e){
+    alert("Canvas not supported!");
+  }
 }
 
-function drawImage() {
+StandLock.prototype.render = function() {
+  
+  this.img = new Image();
+  this.img.onload = this._draw.bind(this);
+  this.img.src = 'progress-tiles.jpg';
 
-    // Draw the base image - no progress
-    drawBase(ctx);
+}
 
-    // Draw the progress segment level
-    drawProgress(ctx);
+StandLock.prototype._draw = function() {
+  this._drawBase();
+  this._drawProgress();
+}
 
+StandLock.prototype._drawBase = function() {
+  this.ctx.drawImage(this.img, 0, 0, this._iWIDTH, this._iHEIGHT, 0, 0, this._iWIDTH, this._iHEIGHT);
+}
+
+StandLock.prototype._drawProgress = function() {
+
+  // Calculated x position where the overalyed image should end
+  var x_end = this._x1 + (this._w * this._slider_value) / 100; // relative to slidebar
+
+  this.ctx.drawImage(this.img, 0, this._iHEIGHT, x_end, this._iHEIGHT, 0, 0, x_end, this._iHEIGHT);
+
+  // Text to screen
+  this.ctx.fillStyle = "grey";
+  this.ctx.font = "14pt Arial";
+  this.ctx.fillText(Math.round(this._slider_value) + " %", this._x_text, this._y_text);
+
+  /* Draw cursor */
+  this.ctx.beginPath();
+  this.ctx.arc(x_end, this._y, this._cursor_radius, 0, 2 * Math.PI, false);
+  var radgrad = this.ctx.createRadialGradient(x_end, this._y, 0, x_end, this._y, this._cursor_radius);
+  radgrad.addColorStop(0, 'hsl(0,0%,85%)');
+  radgrad.addColorStop(0.7, 'hsl(0,0%,80%)');
+  radgrad.addColorStop(0.9, 'hsl(0,0%,80%)');
+  radgrad.addColorStop(1, 'hsl(0,0%,65%)');
+  this.ctx.fillStyle = radgrad;
+  this.ctx.fill();
 }
 
 // Returns mouse position relatively to slidebar
-function getMousePos(canvas, evt) {
-
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left - x1,
-        y: evt.clientY - rect.top - y
-    };
+StandLock.prototype._getMousePos = function(evt) {
+  var rect = this.canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left - this._x1,
+    y: evt.clientY - rect.top - this._y
+  };
 }
 
-function init() {
-
-    canvas = document.getElementById('progress');
-
-    // Create the image resource
-    img = new Image();
-
-    // Canvas supported?
-    if (canvas.getContext) {
-
-        ctx = canvas.getContext('2d');
-        img.onload = drawImage;
-        img.src = 'progress-tiles.jpg';
-
-        var sx = w * slider_value / 100; 
-
-        canvas.addEventListener('mousedown', function(evt) {
-            var mousePos = getMousePos(canvas, evt);
-            //console.debug("mouse DOWN: ", 0, '<=', mousePos.x, '<=', w, '&&', -cursor_radius, '<=', mousePos.y, '<=', cursor_radius);
-
-            // Click is not on the bar
-            if (!((mousePos.y >= -cursor_radius) && (mousePos.y <= cursor_radius))) {
-                return;
-            }
-            else if (passed === true) {
-                return;
-            }
-
-            // Slidelock behavior: need to catch the cursor itself to make it move.
-            if ((mousePos.x >= sx-20) && (mousePos.x <= sx+20)) {
-                cursor_catched = true;
-            }
-            
-            // In standard cursors, the mouse used to catch the cursors anywhere
-            // it is as soon as you click on the bar.
-            // If you want to get the same behavior as standard cursors, 
-            // just replace the conditional block above by the one following.
-            // if  ((mousePos.x >= 0) && (mousePos.x <= w)){
-            //     cursor_catched = true;
-            //     slider_value = Math.round(mousePos.x / w * 100 * 100) / 100;
-            //     // force redraw so that the cursor is 'catched' by the click
-            //     drawImage(ctx);
-            // }
-            }, false);
-
-        canvas.addEventListener('mousemove', function(evt) {
-            if ((cursor_catched == false) || (passed === true)) {
-                return;
-            }
-            var mousePos = getMousePos(canvas, evt);
-            var s = slider_value; // slider value is in percent
-            s = Math.round(mousePos.x / w * 100 * 100) / 100; // rounded to two decimals
-            if (s < 0.0) {
-                s = 0.0;
-            }
-            if (s > 100.0) { // Access granted!
-                s = 100.0;
-                cursor_catched = false;
-                passed = true;
-                ///////////////////////////////////////
-                ///// Launch secure commands here /////
-                securedAction();
-                ///////////////////////////////////////
-            } 
-            // Update slider value
-            slider_value = s;
-            // force redraw so that the progress bar follows the moving cursor
-            drawImage(ctx);
-            }, false);
-
-        canvas.addEventListener('mouseup', function(evt) {
-            cursor_catched = false;
-            if (passed === true) {
-                return;
-            }
-
-            if (slider_value !== 100.0) {
-                // In a slidelock, the cursor used to go back to original position
-                // on mouse up.
-                slider_value = 0;
-                // force redraw so that the cursor is back to origin
-                drawImage(ctx);
-            }
-            }, false);
-
-        canvas.addEventListener('mouseout', function(evt) {
-            cursor_catched = false;
-            if (passed === true) {
-                return;
-            }
-            slider_value = 0;
-            // force redraw so that the cursor is back to origin
-            drawImage(ctx);
-            }, false);
-    } else {
-        alert("Canvas not supported!");
-    }
+StandLock.prototype._onmousemove = function(evt) {
+  if ((this._cursor_catched == false) || (this._passed === true)) {
+    return;
+  }
+  var mousePos = this._getMousePos(evt);
+  var s = this._slider_value; // slider value is in percent
+  s = Math.round(mousePos.x / this._w * 100 * 100) / 100; // rounded to two decimals
+  if (s < 0.0) {
+    s = 0.0;
+  }
+  if (s > 100.0) { // Access granted!
+    s = 100.0;
+    this._cursor_catched = false;
+    this._passed = true;
+    ///////////////////////////////////////
+    ///// Launch secure commands here /////
+    this._unlock();
+    ///////////////////////////////////////
+  }
+  // Update slider value
+  this._slider_value = s;
+  // force redraw so that the progress bar follows the moving cursor
+  this._draw();
 }
 
-function securedAction() {
+StandLock.prototype._onmouseup = function(evt) {
+  this._cursor_catched = false;
+  if (this._passed === true) {
+    return false;
+  }
 
-    // 1- Store encrypted info, e.g. here just the Base64 encoding
-    // of an email address [obtainded with window.btoa()]
-    var bm = "cHJpdmF0ZUBleGFtcGxlLmNvbQ==";
-    var bp = "MSg1NTUpNTU1LTU1NTU=";
-
-    // 2- Implement your own 'decrypt' function here
-    function decrypt(encrypted_msg) {
-        decrypted_msg = window.atob(encrypted_msg);
-        return decrypted_msg;
-    }
-
-    // 3- Safely insert sensitive information in your HTML document
-    insertion  = '<p>';
-    insertion += '<a href="mailto:' + decrypt(bm) + '">' + decrypt(bm) + '</a>';
-    insertion += '&nbsp;&nbsp;|&nbsp;&nbsp;';
-    insertion += '<a href="tel:' + decrypt(bp) + '">' + decrypt(bp) + '</a>';
-    insertion += '</p>';
-    document.getElementById('contact').innerHTML = insertion;
+  if (this._slider_value !== 100.0) {
+    // In a slidelock, the cursor used to go back to original position
+    // on mouse up.
+    this._slider_value = 0;
+    // force redraw so that the cursor is back to origin
+    this._draw();
+  }
 }
 
-//////////////////////////////////
-//// Auto-init on script load ////
-if (document.getElementById('contact')) {
-    init();
+StandLock.prototype._onmousedown = function(evt) {
+  var mousePos = this._getMousePos(evt);
+
+  // Click is not on the bar
+  if (!((mousePos.y >= -this._cursor_radius) && (mousePos.y <= this._cursor_radius))) {
+    return false;
+  }
+  else if (this._passed === true) {
+    return false;
+  }
+
+  // Slidelock behavior: need to catch the cursor itself to make it move.
+  if ((mousePos.x >= this._sx-20) && (mousePos.x <= this._sx+20)) {
+    this._cursor_catched = true;
+  }
+
+  // In standard cursors, the mouse used to catch the cursors anywhere
+  // it is as soon as you click on the bar.
+  // If you want to get the same behavior as standard cursors,
+  // just replace the conditional block above by the one following.
+  // if  ((mousePos.x >= 0) && (mousePos.x <= w)){
+  //     _cursor_catched = true;
+  //     _slider_value = Math.round(mousePos.x / w * 100 * 100) / 100;
+  //     // force redraw so that the cursor is 'catched' by the click
+  //     drawImage(ctx);
+  // }
 }
-//////////////////////////////////
+
+StandLock.prototype._onmouseout = function(evt) {
+  this._cursor_catched = false;
+  if (this._passed === true) {
+    return false;
+  }
+  this._slider_value = 0;
+  this._draw();
+}
+
+StandLock.prototype._unlock = function() {  
+  var o = {};
+  for(var d in this.data){
+    o[d] = this.decryptFn(this.data[d])
+  }
+  this._applyTemplate(this.template, o);
+}
+
+StandLock.prototype._applyTemplate = function(template, obj){
+  for(var val in obj){
+    template = template.replace(new RegExp('{{'+val+'}}', 'g'), obj[val]);
+  }
+  this.placeholder.innerHTML = template;
+}
